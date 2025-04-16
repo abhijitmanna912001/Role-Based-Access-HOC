75a7d3e8022ac7e2bf625d8a6a97d8a285ddb20a
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-16 10:33:56 +0530

    Challenge Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import MyComponent from "./components/MyComponent";
+import NonAuthorized from "./components/NonAuthorized";
+import { withAccessControl } from "./utils/withAccessControl";
+
+const MyComponentWithAccessControl = withAccessControl(MyComponent);
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <MyComponentWithAccessControl
+        roles={["admin", "manager"]}
+        // roles={["user", "guest"]}
+        fallbackComponent={NonAuthorized}
+        message="Hello, Admin!"
+        injectedProps={{
+          userName: "Abhijit Manna",
+          userPermissions: ["view_dashboard", "edit_settings"],
+        }}
+      />
+    </div>
+  );
 };
 
 export default App;
